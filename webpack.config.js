3766904de869dedcafc16bf4a3a35a3712142e29
Rodrigo Costanzo <yodri@Rodrigos-MacBook-Pro-2.local>
2020-09-19 00:01:21 -0300

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
